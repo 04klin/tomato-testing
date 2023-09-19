f5f2c4387f99ad7dfef23dd7db7995072c09edfd
Kevin Lin <2004klin@gmail.com>
2023-09-19 18:43:12 -0400

    Update about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -6,7 +6,7 @@ const About = () => {
     return (        
       <div className="container">
         <h1 className="header">About</h1>
-        <p>In Progress ...</p>
+        <p>This is a website for a research project. Development is still in progess..</p>
       </div>        
     );
 };
