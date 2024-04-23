4d24b5361cf124a2058e3f7c163542be39c13634
Kevin Lin <klin5@ncsu.edu>
2024-04-22 23:12:50 -0400

    Update placeholders

--- client/src/layouts/profile/index.js
+++ client/src/layouts/profile/index.js
@@ -50,11 +50,11 @@ import team2 from "assets/images/team-2.jpg";
 import team3 from "assets/images/team-3.jpg";
 import team4 from "assets/images/team-4.jpg";
 
+import { useAuthContext } from "hooks/useAuthContext";
 
-import { useSoftUIController } from "context";
 
 function Overview() {
-  const { user } = useSoftUIController()
+  const { user } = useAuthContext();
   return (
 
     <DashboardLayout>
